6054ff50732b88e09f84a1019ab3b9b3f31c18d4
Abdulhamid Ibrahim <hamidibrahim2003@gmail.com>
2021-02-23 14:11:01 -0600

    Volume of a Cylider

---
+++ w1d2dataTypes/volumeOfCylinder.js
@@ -0,0 +1,10 @@
+// Write a program that computes volume of a cylinder based on user
+//inputs for radius and height of a cylinder, using formula v = πr2h
+
+const prompt = require ("prompt-sync")();
+let radiusOfCylinder = prompt("please enter the radius of a cylinder?: ")
+let hegitOfCylinder = prompt("please enter the height of a cylinder?: ")
+
+const volume = Math.PI * radiusOfCylinder * radiusOfCylinder * hegitOfCylinder
+console.log(`The Volume Of a Cylider is :  ${volume}`)
+
