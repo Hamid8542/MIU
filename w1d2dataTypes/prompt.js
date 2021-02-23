1df35f43bdf0d0f6012efdc3b58463565de5eb7d
Abdulhamid Ibrahim <hamidibrahim2003@gmail.com>
2021-02-23 15:11:02 -0600

    Exercise with "prompt"

--- w1d2dataTypes/prompt.js
+++ w1d2dataTypes/prompt.js
@@ -1,3 +1,4 @@
+
 const prompt = require ("prompt-sync")();
 let name = prompt("What is your name?: ")
 console.log(`Hi ${name}`)
@@ -5,3 +6,15 @@ console.log(`Hi ${name}`)
 let tempInCelsius = prompt('Eneter value in celsius: ')
 let tempInFahrenheit = 9/5*parseFloat(tempInCelsius)+32;
 console.log(tempInFahrenheit);
+
+
+
+
+// Write a program that computes volume of a cylinder based on user
+//inputs for radius and height of a cylinder, using formula v = πr2h
+
+
+
+// Write a program that takes x and y co-ordinates of two points as
+//inputs and computes distance between these two points based on
+//the formula, d = √( x 2 − x 1 ) 2 + ( y 2 − y 1 ) 2
