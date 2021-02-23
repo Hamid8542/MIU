56c8a2c03a08ce8286592ca04b973c00a2a7ae27
Abdulhamid Ibrahim <hamidibrahim2003@gmail.com>
2021-02-23 15:15:24 -0600

    Assinment for w1d2

---
+++ w1d2dataTypes/w1d2assignment.js
@@ -0,0 +1,31 @@
+/*1. Write a JavaScript program that ask a user for a volume in quarts then converts that value in
+liters. */
+
+
+
+/*2. Write a JavaScript program that asks a user for a distance in kilometers and then convert that
+value in miles.*/
+
+
+/*3. An employee at a grocery store must frequently place boxes of cans in stacks. Write a program
+that allows him to enter the total number of boxes and the number of boxes he will place in
+each stack. Your program must output the number of stacks he will have to make. All the stacks
+except the last one must have the exact number of boxes that the employee specifies. The last
+stack must have the exact number or fewer boxes. For example, if the employee enters 74 total
+boxes and 6 boxes in each stack your program must output 13.
+
+*/
+
+
+
+/*4. Write JavaScript program to compute the mileage of a vehicle. Your program should allow the
+user to enter the beginning and ending odometer readings and the number of gallons of gas
+used and should output the mileage in miles per gallon.*/
+
+
+/*5. When you exercise to strengthen your heart, you should maintain your heart rate within a
+range. To find that range, subtract your age from 220. This difference is your maximum heart
+rate per minute. Your heart simply will not beat faster than this maximum (220 − age). When
+exercising to strengthen your heart, you should keep your heart rate between 65% and 85% of
+your heart’s maximum. Write a JavaScript program that asks for a person’s age and computes
+and outputs the slowest and fastest rates necessary to strengthen his heart.*/
