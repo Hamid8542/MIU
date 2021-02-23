45aefc5db1dce9bd2b2c4934a83bd0d2536f1ba1
Abdulhamid Ibrahim <hamidibrahim2003@gmail.com>
2021-02-22 21:52:37 -0600

    w1d1 home work as a text file

---
+++ w1d1algorithms/w1d1textfile.js
@@ -0,0 +1,105 @@
+W1D1 Algorithms assignment: 
+
+Read contents from referenced links. 
+
+What happen when an assignment is performed without using let or const keywords in strict mode? 
+
+Ans.  
+
+ It will generate an error 
+
+ 
+
+List 5 reserved keywords in JavaScript 
+
+ 
+
+Ans.  
+
+var, let, const, alert, return, for, do, while, break, if, else, while, typeof, switch, function, true, false, … 
+
+ 
+
+How is a computer program similar / different than an algorithm? 
+
+             Ans 
+
+A computer program is. 
+
+ 
+
+Sequence of instructions. 
+
+It complies specific rules.  
+
+Each computer language has their own specific syntax or rules to follow. 
+
+             Whereas An algorithm is  
+
+Step by step set of operations to perform specific task or problem.  
+
+Algorithms can be expressed using natural language, flowcharts, etc. 
+
+No need to comply specific rules.  
+
+ 
+
+Write at least two algorithms you perform on your daily life, with at least one algorithm that can be converted to computer program. 
+
+ 
+
+Algorithm 1. 
+
+ 
+
+Wakeup around 6 am in the morning every day. 
+
+Brash my teeth. 
+
+Morning prays. 
+
+Do 10 to 15 min exercise. 
+
+Take a shower. 
+
+Morning meditation 
+
+Make and have breakfast. 
+
+Check email. 
+
+Check weather I have a leftover assignment from the other night? 
+
+If there is a leftover assignment, complete the assignment. 
+
+If not prepare for today’s class 
+
+ 
+
+ 
+
+ 
+
+Algorithm 2 
+
+ 
+
+Everyday around 5:15 pm have a launch with my family. 
+
+Spent some time with my family. 
+
+If I need to call, call, and return the phone call. 
+
+Around 6:30 pm focus on my study.  
+
+Check email and Sakai website to check assignment for the day. 
+
+If the assignment can do easily, do the assignment. 
+
+If not read related material  
+
+If I can complete the assignment before 9:00 pm submit the assignment 
+
+If not leave the assignment for the next day  
+
+Prepare myself for bed  
