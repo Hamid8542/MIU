412a71d70e38dba6ace87df1207605c79b9bdad7
Abdulhamid Ibrahim <hamidibrahim2003@gmail.com>
2021-02-23 22:04:02 -0600

    w1d2promptexercise

---
+++ check.js
@@ -0,0 +1,11 @@
+//let a = b = 2;
+  //let c = 3 - (a + b + 1);
+//  console.log(a--);
+//  console.log(++c);
+//  console.log(a);
+//  console.log(c);
+
+  console.log(Number(null));
+   console.log(Number(undefined));
+   console.log(Boolean("0"));
+   console.log(Boolean(" "))
