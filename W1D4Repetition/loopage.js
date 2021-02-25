e7a07ff32099d337f8df7f858edcaed0e2d85ddd
Abdulhamid Ibrahim <hamidibrahim2003@gmail.com>
2021-02-25 13:03:25 -0600

    w1d4 Class exercise on Repition

--- W1D4Repetition/loopage.js
+++ W1D4Repetition/loopage.js
@@ -3,3 +3,26 @@ Write a loop that keeps on prompting for age until you enter age
 older than 18
 • Write both while and do while versions.
 */
+/*
+// using while loop
+const prompt = require('prompt-sync')();
+let age = prompt('Enter your Age please  : ');
+ 
+while(age <= 18 ){
+    console.log(" please try again , You are below 18 ")
+    age = prompt('Enter your Age please  : ');
+  
+}
+    console.log(" you are over 18 ")
+*/
+
+//using do loop
+const prompt = require('prompt-sync')();
+let age = prompt('Enter your Age please  : ');
+ 
+while(age <= 18 ){
+    console.log(" please try again , You are below 18 ")
+    age = prompt('Enter your Age please  : ');
+  
+}
+    console.log(" you are over 18 ")
