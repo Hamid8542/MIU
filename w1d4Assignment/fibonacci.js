5cc348501f75962a09694b331f61f1fe769952ba
Abdulhamid Ibrahim <hamidibrahim2003@gmail.com>
2021-02-25 21:18:07 -0600

    W1D4 Assignment

--- w1d4Assignment/fibonacci.js
+++ w1d4Assignment/fibonacci.js
@@ -4,8 +4,8 @@
    The Fibonacci series is a sequence such that each number is the sum of the two preceding ones,
    starting from 0 and 1. That is, fib(0) = 0, fib(1) = 1, fib(n) = fib(n – 1) + fib(n – 2) for n > 1.
    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …
-
 */
+   // Defining Table 
 /*
     input : Positive Number
     output: Fibonacci Series
