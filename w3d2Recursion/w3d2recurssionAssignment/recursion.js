24c8ec9560af0c4bf364c63bc4a63660c8142fb8
Abdulhamid Ibrahim <hamidibrahim2003@gmail.com>
2021-03-09 15:49:37 -0600

    commit

--- w3d2Recursion/w3d2recurssionAssignment/recursion.js
+++ w3d2Recursion/w3d2recurssionAssignment/recursion.js
@@ -23,16 +23,29 @@ function countDigits(number){
     * @returns {String}
     */
 
-   function reverse(str) {
-    if (str === "")            // This is the terminal case that will end the recursion
-      return "";
-    
-    else
-    
-      return reverse(str.substr(1)) + str.charAt(0);
-}
-    console.log(reverse("hello"));
+// method --- > 1
 
+//    function reverse(str) {
+//     if (str === "")            // This is the terminal case that will end the recursion
+//       return "";
+    
+//     else
+    
+//       return reverse(str.substr(1)) + str.charAt(0);
+// }
+//     console.log(reverse("hello"));
+
+// method -- 2
+
+function reverse(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) { 
+        newString += str[i]; // or newString = newString + str[i];
+    }
+    return newString; // "olleh"
+}
+ 
+console.log(reverse('hello'));
 
 /**
  * 
@@ -54,3 +67,7 @@ function countDigits(number){
     };
     
         console.log(power(4, 4));
+
+
+
+      
